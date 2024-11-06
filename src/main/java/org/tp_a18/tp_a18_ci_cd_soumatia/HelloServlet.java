4142c6855519ea2dbf04580bd285175bebdfdd25
Fatma_Zohra_SOUMATIA <6283978@bdeb.qc.ca>
2024-11-06 15:11:34 -0500

    premiere mise a jour

---
+++ src/main/java/org/tp_a18/tp_a18_ci_cd_soumatia/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.tp_a18.tp_a18_ci_cd_soumatia;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
