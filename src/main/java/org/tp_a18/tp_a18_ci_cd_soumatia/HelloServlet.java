274c1396b47737502dbe477038713edf12f5adc5
Fatma_Zohra_SOUMATIA <6283978@bdeb.qc.ca>
2024-11-08 11:50:53 -0500

    mise a jour4 servlet

--- src/main/java/org/tp_a18/tp_a18_ci_cd_soumatia/HelloServlet.java
+++ src/main/java/org/tp_a18/tp_a18_ci_cd_soumatia/HelloServlet.java
@@ -10,7 +10,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "premiere servlet";
+        message = "premiere servlet ";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
